b2181f68b4b05bed21d2aeba687e29cdf4d97f9b
Pakinackermen <actionsisty@gmail.com>
2019-03-14 23:38:04 +0700

    edit

--- backup_recovery/backend/backup/linealert.php
+++ backup_recovery/backend/backup/linealert.php
@@ -5,9 +5,7 @@
     // $access_token = 'D1Uyqtjz5ejEYtHzGNXFhfQmLTNQFEHk1a2DSzhsjf4';
     $access_token = $token;
 
-    $str = "Backup :: \n".$path;    //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
-    $str .= "reduce :: ...\n";
-    $str .= "new :: ....\n"  ;
+    $str = "Backup :: \n".$path;    //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร    
     $image_thumbnail_url = '';  // ขนาดสูงสุด 240×240px JPEG
     $image_fullsize_url = '';  // ขนาดสูงสุด 1024×1024px JPEG
     $sticker_package_id = '';  // Package ID ของสติกเกอร์
