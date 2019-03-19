66c0fa31f598c25c7a1b3692c8b68d53d2e670a0
Pakinackermen <actionsisty@gmail.com>
2019-03-20 02:45:45 +0700

    Understand

--- backup_recovery/backend/backup/backupindex.php
+++ backup_recovery/backend/backup/backupindex.php
@@ -5,12 +5,12 @@ include_once 'C:\xampp\htdocs\backup_recovery\backend\config\ftp.php';
 include_once 'C:\xampp\htdocs\backup_recovery\backend\checkData\checkNewFile.php';
 
 // set not show wanning and error
-// ini_set('log_errors', 'On');
-// ini_set('display_errors', 'Off');
-// ini_set('error_reporting', E_ALL);
-// define('WP_DEBUG', false);
-// define('WP_DEBUG_LOG', true);
-// define('WP_DEBUG_DISPLAY', false);
+ini_set('log_errors', 'On');
+ini_set('display_errors', 'Off');
+ini_set('error_reporting', E_ALL);
+define('WP_DEBUG', false);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
 $idSetting = $_POST['idSetting'];
 $id_ftp = $_POST['id_ftp'];
